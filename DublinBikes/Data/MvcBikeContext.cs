1c46c4ec27d07cefe180687f47f81302812aa9a6
louis.ribault <louis.ribault@epita.fr>
2021-03-21 16:05:20 +0000

    with context

---
+++ DublinBikes/Data/MvcBikeContext.cs
@@ -0,0 +1,19 @@
+﻿using DublinBikes.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DublinBikes.Data
+{
+    public class MvcBikeContext : DbContext
+    {
+        public MvcBikeContext(DbContextOptions<MvcBikeContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<DublinBike> DublinBike { get; set; }
+    }
+}
