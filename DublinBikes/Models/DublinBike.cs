1cc96d207506e56bfa762002f73a082700384c62
Anjara Rabesandratana <anjara.rabesandratana@epita.fr>
2021-03-21 17:03:02 +0000

    Seed Data, DublinBike

--- DublinBikes/Models/DublinBike.cs
+++ DublinBikes/Models/DublinBike.cs
@@ -1,17 +1,55 @@
-﻿namespace DublinBikes.Models
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+namespace DublinBikes.Models
 {
     public class DublinBike
     {
+        [Required]
         public int Number { get; set; }
+
+        [Required]
+        [StringLength(60, MinimumLength = 3)]
         public string ContractName { get; set; }
+
+        [Required]
+        [StringLength(60, MinimumLength = 3)]
         public string Name { get; set; }
+
+       
+        [Required]
+        [StringLength(30)]
         public string Address { get; set; }
+
+        [Required]
         public float Latitude { get; set; }
+
+        [Required]
         public float Longitude { get; set; }
+
+
+        [Required]
         public bool Banking { get; set; }
+
+        [Required]
+        [Range(1, 100)]
         public int Available_bikes { get; set; }
+
+        [Required]
+        [Range(1, 100)]
         public int Available_stands { get; set; }
+
+        [Required]
+        [Range(1, 100)]
         public int Capacity { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z]*$")]
+        [Required]
+        [StringLength(30)]
         public string Status { get; set; }
     }
+
 }
---
+++ DublinBikes/Models/SeedData.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using DublinBikes.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DublinBikes.Models
+{
+    public static class SeedData
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+    {
+        using (var context = new MvcBikeContext(
+            serviceProvider.GetRequiredService<
+                DbContextOptions<MvcBikeContext>>()))
+        {
+            // Look for any station.
+            if (context.DublinBike.Any())
+            {
+                return;   // DB has been seeded
+            }
+
+                context.DublinBike.AddRange(
+                    new DublinBike
+                    {
+                        Number = 42,
+                        ContractName = "Dublin",
+                        Name = "SMITHFIELD NORTH",
+                        Address = "Smithfield North",
+                        Latitude = 53.349562f,
+                        Longitude = -6.278198f,
+                        Banking = true,
+                        Status = "OPEN",
+                        Available_bikes = 40,
+                        Available_stands = 10,
+                        Capacity = 50
+                    },
+
+
+
+                    new DublinBike
+                    {
+                        Number = 30,
+                        ContractName = "Dublin",
+                        Name = "PARNELL SQUARE NORTH",
+                        Address = "Parnell Square North",
+                        Latitude = 53.353462f,
+                        Longitude = -6.265305f,
+                        Banking = true,
+                        Status = "OPEN",
+                        Available_bikes = 40,
+                        Available_stands = 10,
+                        Capacity = 50
+                    },
+
+                    new DublinBike
+                    {
+                        Number = 54,
+                        ContractName = "Dublin",
+                        Name = "CLONMEL STREET",
+                        Address = "Clonmel Street",
+                        Latitude = 53.98696f,
+                        Longitude = -6.275879f,
+                        Banking = true,
+                        Status = "OPEN",
+                        Available_bikes = 40,
+                        Available_stands = 10,
+                        Capacity = 50
+                    },
+
+                    new DublinBike
+                    {
+                        Number = 48,
+                        ContractName = "Dublin",
+                        Name = "EXCISE WALK",
+                        Address = "Excise Walk",
+                        Latitude = 53.207402f,
+                        Longitude = -6.535775f,
+                        Banking = true,
+                        Status = "OPEN",
+                        Available_bikes = 40,
+                        Available_stands = 10,
+                        Capacity = 50
+                    }
+                ) ;
+            context.SaveChanges();
+        }
+    }
+}
+}
